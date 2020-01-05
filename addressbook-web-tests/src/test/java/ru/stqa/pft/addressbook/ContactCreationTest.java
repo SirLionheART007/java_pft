658e8e928d38141c11839149526d9a6a7b14b7b6
SirLionheART007 <52289447+SirLionheART007@users.noreply.github.com>
2020-01-06 01:20:39 +0300

    Homework4 - Замена class на method

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 public class ContactCreationTest {
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -78,7 +78,7 @@ public class ContactCreationTest {
     wd.findElement(By.linkText("add new")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
   }
