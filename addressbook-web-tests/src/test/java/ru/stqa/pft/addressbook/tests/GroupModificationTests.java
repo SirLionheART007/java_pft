2bd3f83af5ca3ca21b38dc14b5a60bc1ef96fd28
SirLionheART007 <52289447+SirLionheART007@users.noreply.github.com>
2020-01-12 23:18:55 +0300

    Множества

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
@@ -10,12 +14,21 @@ public class GroupModificationTests extends TestBase {
 
     app.getNavigationHelper().gotoGroupPage("groups");
     if (! app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test2", "test3", "test4"));
+      app.getGroupHelper().createGroup(new GroupData("test2", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test2", "test3", "test4");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
   }
 }
